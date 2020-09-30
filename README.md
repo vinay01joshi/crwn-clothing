e0ad359a6a47055fa237a1ff55d245e86010967a
Vinay Joshi <vinay01joshi@gmail.com>
2020-09-30 15:12:47 +0530

    Introduce Routing in Project

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This is a E-Commerce Application UI based on React component.
 ---
 
 - [node-sass](https://www.npmjs.com/package/node-sass) `Package used to support sass styles sheet in node project`
+- [react-router-dom](https://www.npmjs.com/package/react-router-dom) `Packge used to add Routing feature in React Project`
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/home/homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
