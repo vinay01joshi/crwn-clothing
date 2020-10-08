0f14ae2acd177e4d782bb2423b3f0d7466a71c0d
Vinay Joshi <vinay01joshi@gmail.com>
2020-10-08 13:34:46 +0530

    Adding FormInput Common component which is custom input text

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import FormInput from '../form-input/form-input.component'
 import './sign-in.styles.scss'
 
 class SignIn extends React.Component {
@@ -19,14 +19,14 @@ class SignIn extends React.Component {
                 <h2 className='title'>I already have an Account</h2>
                 <span>Sign-In with your email and passwrod</span>
                 <form>
-                    <input
+                    <FormInput
                         type='email'
                         name='email'
                         value={this.state.email}
                         label='Email'
                         required
                     />
-                    <input
+                    <FormInput
                         type='password'
                         name='password'
                         value={this.state.password}
