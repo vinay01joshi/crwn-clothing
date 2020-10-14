f82878bbf8b0de6fb5cc72941c9926e348eb974f
Vinay Joshi <vinay01joshi@gmail.com>
2020-10-14 13:59:52 +0530

    Removing and Item on Quantity Arrow.

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGELE_CART_HIDDEN: 'TOGGELE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+    REMOVE_ITEM: 'REMOVE_TEIM'
 };
 
 export default CartActionTypes;
