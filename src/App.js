79c061839b092831f850a72c666512eb9f5db0a7
Vinay Joshi <vinay01joshi@gmail.com>
2021-01-22 13:53:44 +0530

    Removing lifecycle method with userEffect React Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './components/header/header.component'
@@ -9,32 +9,25 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 import './App.css';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.action'
-class App extends React.Component {
-  unSubscribeFromAuth = null;
+const App = ({ checkUserSession, currentUser }) => {
 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
+  useEffect(() => {
+    checkUserSession()
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unSubscribeFromAuth();
-  }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin'
-            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
-        </Switch>
-      </div >
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route exact path='/signin'
+          render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
+      </Switch>
+    </div >
+  );
 }
 
 const mapStateToProps = (state) => ({
--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchCollectionStart } from '../../redux/shop/shop.actions';
 import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
+const ShopPage = ({ fetchCollectionStart, match }) => {
 
-    componentDidMount() {
-        const { fetchCollectionStart } = this.props;
-        fetchCollectionStart();
-    }
+    useEffect(() => {
+        fetchCollectionStart()
+    }, [fetchCollectionStart])
 
-    render() {
-        const { match } = this.props;
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-            </div>
-        );
-    }
+
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+        </div>
+    );
 }
+
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionStart: () => dispatch(fetchCollectionStart())
